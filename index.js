b1ffa097fde045cccf400cb0e01242f26ee7a5f0
omondimaurine <omondimaurine713@gmail.com>
2024-03-25 10:37:17 +0300

    counting numbers with javascript

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function  writeCards(namesArray, events){
+    let thankYouMessages = [];
+
+    for(let i = 0; i < namesArray.length; i++){
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+    
+    return thankYouMessages;
+
+
+}
+
+function countDown(startingNumber){
+    
+    while( startingNumber> 0){
+        console.log(startingNumber);
+        startingNumber -=1;
+    }
+
+    console.log(startingNumber);
+
+
+}
